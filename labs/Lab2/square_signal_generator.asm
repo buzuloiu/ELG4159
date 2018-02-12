af31aac1722ab7c07b47f194b0912f4b168f4499
Paul Buzuloiu <pbuzuloiu@yahoo.ca>
2018-02-12 15:04:31 -0500

    debug lab 2

--- labs/Lab2/square_signal_generator.asm
+++ labs/Lab2/square_signal_generator.asm
@@ -5,47 +5,59 @@
 
 ; variable declarations
 
-TIMER_INIT EQ 0x7D ; 125
+TIMER_INIT EQU d'125'
 
+ 
+ org 0x00
+ goto initialize
+ org 0x05
+ 
 initialize
-; select internal clock for timer0
-  BCF OPTION_REG, 5
-; select pre-scaler for timer0 to 32
-  BCF OPTION_REG, 4
-; configure pre-scaler 2^32
-  BSF OPTION_REG, 0
-  BCF OPTION_REG, 1
-  BSF OPTION_REG, 2
-; set clock to 125KHz
-  BSF STATUS, 0
-  BSF OSCON, 4
-  BCF OSCON, 5
-  BCF OSCON, 6
-; configure RA0 as output
-  BCF TRISA, 0
-; disable comparators
-  MOVLW 0x07
-  MOVWF CMCON0
-; return to Bank 0
-  BCF STATUS, 0
-  GOTO main
+    ; select internal clock for timer0
+      BCF OPTION_REG,5
+    ; select pre-scaler for timer0 to 32
+      BCF OPTION_REG,4
+    ; configure pre-scaler 2^32
+      BSF OPTION_REG,0
+      BCF OPTION_REG,1
+      BSF OPTION_REG,2
+
+
+    ; set clock to 125KHz
+      BSF STATUS,0
+      BSF OSCCON,4
+      BCF OSCCON,5
+      BCF OSCCON,6
+    ; configure RA0 as output
+      BCF TRISA,0
+    ; disable comparators
+      MOVLW 0x07
+      MOVWF CMCON0
+    ; return to Bank 0
+      BCF STATUS,0
+      BSF INTCON, 5
+      GOTO main
 
 main
-  MOVLW TMR0, TIMER_INIT
-  BSF INTCON, 5
-  GOTO wait
+    BCF INTCON,T0IF
+    MOVLW TIMER_INIT
+    MOVWF TMR0
+    GOTO wait
+  
+terminate
+     GOTO terminate
 
+  
 wait
-  BTSS INTCON, 2
-  GOTO wait
-; reset interrupt
-  BCF INTCON, 5
-  BCF INTCON, 2
-  GOTO toggle
+    BTFSS INTCON,T0IF
+    GOTO wait
+    GOTO toggle
 
-toggle
-  BTSS PORTA, 0
-  BCF PORTA, 0
-  BTSC PORTA, 0
-  BSF PORTA, 0
-  GOTO main
+
+toggle	
+    MOVLW b'00000001'
+    XORWF PORTA,1
+    GOTO main
+
+  
+END
