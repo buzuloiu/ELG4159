a22954194b83baf65a1f99702a4f778460bfab7e
Paul Buzuloiu <pbuzuloiu@yahoo.ca>
2018-03-06 18:10:34 -0500

    more prelab for lab 3

---
+++ labs/Lab3/Lab_3.X/lab_3.asm
@@ -0,0 +1,54 @@
+__CONFIG _CP_OFF & _CPD_OFF & _BOD_OFF & _PWRTE_ON & _WDT_OFF & _INTRC_OSC_NOCLKOUT & _MCLRE_ON & _FCMEN_OFF & _IESO_OFF
+errorlevel -302 ; supress "register not in bank0, check page bits" message
+    
+    
+#include <p16f917.inc>
+
+    ;group 16
+    ;A is 0
+    ;B is 1
+    ;T is 1 second
+    
+    ;Write an assembly program to continuously sample an analog 
+    ;input signal into a 10-bit binary value. 
+
+    
+    
+    org 0x00
+    goto START
+    org 0x05
+
+START
+    BANKSEL ADCON1 ;
+    MOVLW b'01110000' ;ADC Frc clock
+    MOVWF ADCON1 ;
+    BANKSEL TRISA ;
+    BSF TRISA,0 ;Set RA0 to input
+    BANKSEL ANSEL ;
+    BSF ANSEL,0 ;Set RA0 to analog
+    BANKSEL ADCON0 ;
+    MOVLW b'10000001' ;Right justify,
+    MOVWF ADCON0 ;Vdd Vref, AN0, On
+    CALL SampleTime ;Acquisiton delay
+    
+    
+LOOP
+    BTFSC ADCON0,GO ;Is conversion done?
+    GOTO LOOP;No, test again
+    call PROCESS_DATA
+    BSF ADCON0,GO ;Start conversion again
+    GOTO LOOP
+
+    
+TERMINATE
+    goto TERMINATE
+
+PROCESS_DATA
+    NOP ;this is where the bits would be read from ADRESH and ADRESL
+    return
+    
+SampleTime
+    NOP ;this is where there would be a delay of time before they start converting
+    return 
+    
+END
