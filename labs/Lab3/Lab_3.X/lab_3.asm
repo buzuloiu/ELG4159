a5e6a9459b7e34ebb582a5fa1c0564a9aed79c98
Paul Buzuloiu <pbuzuloiu@yahoo.ca>
2018-03-08 15:42:24 -0500

    done prelab

--- labs/Lab3/Lab_3.X/lab_3.asm
+++ labs/Lab3/Lab_3.X/lab_3.asm
@@ -1,56 +1,75 @@
-__CONFIG _CP_OFF & _CPD_OFF & _BOD_OFF & _PWRTE_ON & _WDT_OFF & _INTRC_OSC_NOCLKOUT & _MCLRE_ON & _FCMEN_OFF & _IESO_OFF
+;Group 16
+;Paul Buzuloiu
+;Manit Ginoya
+;Nikhil Peri  
+
+;we use pin RA0 because 16 mod 8 is 0    
+    
+;Write an assembly program to continuously sample an analog 
+;input signal into a 10-bit binary value. 
+    
+    __CONFIG _CP_OFF & _CPD_OFF & _BOD_OFF & _PWRTE_ON & _WDT_OFF & _INTRC_OSC_NOCLKOUT & _MCLRE_ON & _FCMEN_OFF & _IESO_OFF
 errorlevel -302 ; supress "register not in bank0, check page bits" message
     
     
-#include <p16f917.inc>
-
-    ;group 16
-    ;A is 0
-    ;B is 1
-    ;T is 1 second
-    
-    ;Write an assembly program to continuously sample an analog 
-    ;input signal into a 10-bit binary value. 
-
-    
+#include <p16f917.inc>  
     
     org 0x00
     goto START
     org 0x05
 
 START
-    BANKSEL ADCON1 ;
-    MOVLW b'01110000' ;ADC Frc clock
-    MOVWF ADCON1 ;
-    BANKSEL TRISA ;
+    BANKSEL ADCON1 
+    MOVLW b'01010000' ;set ADC Prescaler to 16
+    MOVWF ADCON1 
+    
+    BANKSEL TRISA 
     BSF TRISA,0 ;Set RA0 to input
-    BANKSEL ANSEL ;
+    
+    BANKSEL ANSEL 
     BSF ANSEL,0 ;Set RA0 to analog
-    BANKSEL ADCON0 ;
-    MOVLW b'10000001' ;Right justify,
-    MOVWF ADCON0 ;Vdd Vref, AN0, On
-    CALL SampleTime ;Acquisiton delay
+       
+    BANKSEL  OSCCON
+    MOVLW b'01110000' ;Set F_osc to 8MHz
+    MOVWF OSCCON
     
-    
+    BANKSEL OPTION_REG
+    MOVLW b'10000001' ;set timer0 prescaler to 4
+    MOVWF OPTION_REG
+        
+    BANKSEL ADCON0 
+    MOVLW b'10000001' ;Right justify, enable ADC
+    MOVWF ADCON0 
+    CALL SampleTime ; Wait for Acquisiton delay
+       
 LOOP
-    BTFSC ADCON0,GO ;Is conversion done?
-    GOTO LOOP;No, test again
+    BTFSC ADCON0,GO ; Wait until conversion finishes
+    GOTO LOOP
     call PROCESS_DATA
     BSF ADCON0,GO ;Start conversion again
     GOTO LOOP
-
-    
+   
 TERMINATE
     goto TERMINATE
 
 PROCESS_DATA
-    NOP ;this is where the bits would be read from ADRESH and ADRESL
+    NOP 
+    ;this is where the bits would be read from ADRESH and ADRESL
     ;for the purpose of this prelab, I don't think anything further needs to be done here
+    ;the analog signal has been converted to 10 bit digital, stored in ADRESH and ADRSEL
     return
     
-SampleTime
-    NOP ;this is where there would be a delay of time before they start converting
-    ;someone should implement this, including the initialization of timer0 at the beginning
+SampleTime ;acquisition delay subroutine, waits 2 microseconds
+    BANKSEL INTCON ;move to bank 0
+    bcf INTCON,T0IF ;clear timer 1 interrupt flag
+    MOVLW d'255' ;set timer0 initial value
+    MOVWF TMR0
+    call TimingLoop 
     return 
+    
+TimingLoop
+    BTFSS INTCON,T0IF ;wait until timer overflows (2 microseconds)
+    goto TimingLoop
+    return
     
 END
---
+++ practice_problems/ADC_DAC/DAC_example_from_slides.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=DAC_example_from_slides.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/DAC_example_from_slides.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=dacexamplefromslides.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/dacexamplefromslides.x.tar
